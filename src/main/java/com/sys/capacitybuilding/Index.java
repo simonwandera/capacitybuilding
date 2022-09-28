3c06b13395035d146996c208d9f19e10d24de8f6
simonwandera <simonwandera12@gmail.com>
2022-09-29 02:14:36 +0300

    -added head elements

--- src/main/java/com/sys/capacitybuilding/Index.java
+++ src/main/java/com/sys/capacitybuilding/Index.java
@@ -16,14 +16,24 @@ public class Index implements Servlet {
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         res.setContentType("text/html");
 
-
         PrintWriter pw = res.getWriter();
-        pw.print("<html>");
+
+        pw.print("<html lang=\"en\">");
         pw.print("<head>");
-        pw.print("<title>HTML 5 Boilerplate</title>");
+        pw.print("<meta charset=\"UTF-8\">");
+        pw.print("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+        pw.print("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        pw.print("<title>Document</title>");
+        pw.print("<link rel=\"stylesheet\" href=\"style.css\">");
+        pw.print("</head>");
+
+
+        pw.print("<head>");
+        pw.print("<title>Capacity building System</title>");
+        pw.print("<link rel=\"stylesheet\" href=\"style.css\">");
         pw.print("</head>");
         pw.print("<body>");
-        pw.print("<p>Do me a favour and please compile </p>");
+        pw.print("<p>Do me a favour and please compile please</p>");
         pw.print("</body>");
         pw.print("</html>");
 
