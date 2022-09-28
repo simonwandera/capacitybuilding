315a754567e8cbdcc3930569ba0112ac2fb8eff3
simonwandera <simonwandera12@gmail.com>
2022-09-29 02:31:10 +0300

    -added the rest of the html

--- src/main/java/com/sys/capacitybuilding/Index.java
+++ src/main/java/com/sys/capacitybuilding/Index.java
@@ -27,13 +27,58 @@ public class Index implements Servlet {
         pw.print("<link rel=\"stylesheet\" href=\"style.css\">");
         pw.print("</head>");
 
-
-        pw.print("<head>");
-        pw.print("<title>Capacity building System</title>");
-        pw.print("<link rel=\"stylesheet\" href=\"style.css\">");
-        pw.print("</head>");
         pw.print("<body>");
-        pw.print("<p>Do me a favour and please compile please</p>");
+        pw.print("<h2>Welcome to capacity buiding system</h2>");
+        pw.print("<div class=\"container\" id=\"container\">");
+        pw.print("<div class=\"form-container sign-up-container\">");
+        pw.print("<form action="#">");
+        pw.print("<h1>Create Account</h1>");
+        pw.print("<div class="social-container">
+        pw.print("<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>");
+        pw.print("<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>");
+        pw.print("<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>");
+        pw.print("</div>");
+        pw.print("<span>or use your email for registration</span>
+        pw.print("<input type="text" placeholder="Name" />");
+        pw.print("<input type="email" placeholder="Email" />");
+        pw.print("<input type="password" placeholder="Password" />");
+        pw.print("<button>Sign Up</button>");
+        pw.print("</form>");
+        pw.print("</div>");
+        pw.print("<div class="form-container sign-in-container">");
+        pw.print("<form action="#">");
+        pw.print("<h1>Sign in</h1>");
+        pw.print("<div class="social-container">");
+        pw.print("<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>");
+        pw.print("<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>");
+        pw.print("<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>");
+        pw.print("</div>");
+        pw.print("<span>or use your account</span>");
+        pw.print("<input type="email" placeholder="Email" />");
+        pw.print("<input type="password" placeholder="Password" />");
+        pw.print("<a href="#">Forgot your password?</a>");
+        pw.print("<button>Sign In</button>");
+        pw.print("</form>");
+        pw.print("</div>");
+        pw.print("<div class="overlay-container">");
+        pw.print("<div class="overlay">");
+        pw.print("<div class="overlay-panel overlay-left">");
+        pw.print("<h1>Welcome Back!</h1>");
+        pw.print("<p>To keep connected with us, login and track your training</p>");
+        pw.print("<button class="ghost" id="signIn">Sign In</button>");
+        pw.print("</div>");
+        pw.print("<div class="overlay-panel overlay-right">");
+        pw.print("<h1>Hello, Friend!</h1>");
+        pw.print("<p>Enter your personal details and start the empowerment journey with us</p>");
+        pw.print("<button class="ghost" id="signUp" name="signUp">Sign Up</button>");
+        pw.print("</div>");
+        pw.print("</div>");
+        pw.print("</div>");
+        pw.print("</div>");
+        pw.print("<footer>");
+        pw.print("<p> Like a battery, the human mind and body must be fully discharged to stretch their capacity. </p>");
+        pw.print("</footer>");
+        pw.print("<script type="text/javascript" src="index.js"></script>");
         pw.print("</body>");
         pw.print("</html>");
 
