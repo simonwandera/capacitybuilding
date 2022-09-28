c89871bac0ea9e6456138024046838aa537c9731
simonwandera <simonwandera12@gmail.com>
2022-09-28 21:49:13 +0300

    -Added responce contentType

--- src/main/java/com/sys/capacitybuilding/index.java
+++ src/main/java/com/sys/capacitybuilding/index.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 
 public class index implements Servlet {
     public void init(ServletConfig servletConfig) throws ServletException {
-        
+
     }
 
     public ServletConfig getServletConfig() {
         return null;
     }
 
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
 
     }
-
     public String getServletInfo() {
         return null;
     }
