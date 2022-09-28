95d7238f2bbb7b52e8bbeacfb9cbe1f26466f4bf
simonwandera <simonwandera12@gmail.com>
2022-09-28 21:43:14 +0300

    -Added index.java to implement servlet

--- src/main/java/com/sys/capacitybuilding/index.java
+++ src/main/java/com/sys/capacitybuilding/index.java
@@ -1,4 +1,26 @@
 package com.sys.capacitybuilding;
 
-public class index {
+import javax.servlet.*;
+import java.io.IOException;
+
+public class index implements Servlet {
+    public void init(ServletConfig servletConfig) throws ServletException {
+        
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
 }
