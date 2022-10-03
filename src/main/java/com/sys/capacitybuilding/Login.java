82a7eccfc9de50393ce9e82bdc916a64aaf125c8
simonwandera <simonwandera12@gmail.com>
2022-10-03 21:21:52 +0300

    mapping of assets to  html

--- src/main/java/com/sys/capacitybuilding/Login.java
+++ src/main/java/com/sys/capacitybuilding/Login.java
@@ -26,13 +26,13 @@ public class Login extends HttpServlet {
                 "  <meta charset=\"utf-8\" />\n" +
                 "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\" />\n" +
                 "  <title>Login</title>\n" +
-                "  <link rel=\"stylesheet\" href=\"assets/vendors/mdi/css/materialdesignicons.min.css\" />\n" +
-                "  <link rel=\"stylesheet\" href=\"assets/vendors/flag-icon-css/css/flag-icon.min.css\" />\n" +
-                "  <link rel=\"stylesheet\" href=\"assets/vendors/css/vendor.bundle.base.css\" />\n" +
-                "  <link rel=\"stylesheet\" href=\"assets/vendors/select2/select2.min.css\" />\n" +
-                "  <link rel=\"stylesheet\" href=\"assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css\" />\n" +
-                "  <link rel=\"stylesheet\" href=\"assets/css/style.css\" />\n" +
-                "  <link rel=\"shortcut icon\" href=\"assets/images/favicon.png\" />\n" +
+                "  <link rel=\"stylesheet\" href=\"./assets/vendors/mdi/css/materialdesignicons.min.css\" />\n" +
+                "  <link rel=\"stylesheet\" href=\"./assets/vendors/flag-icon-css/css/flag-icon.min.css\" />\n" +
+                "  <link rel=\"stylesheet\" href=\"./assets/vendors/css/vendor.bundle.base.css\" />\n" +
+                "  <link rel=\"stylesheet\" href=\"./assets/vendors/select2/select2.min.css\" />\n" +
+                "  <link rel=\"stylesheet\" href=\"./assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css\" />\n" +
+                "  <link rel=\"stylesheet\" href=\"./assets/css/style.css\" />\n" +
+                "  <link rel=\"shortcut icon\" href=\"./assets/images/favicon.png\" />\n" +
                 "</head>\n" +
                 "<body>\n" +
                 "  <div class=\"container-scroller\">\n" +
@@ -75,15 +75,15 @@ public class Login extends HttpServlet {
                 "      </div>\n" +
                 "    </div>\n" +
                 "  </div>\n" +
-                "  <script src=\"assets/vendors/js/vendor.bundle.base.js\"></script>\n" +
-                "  <script src=\"assets/vendors/select2/select2.min.js\"></script>\n" +
-                "  <script src=\"assets/vendors/typeahead.js/typeahead.bundle.min.js\"></script>\n" +
-                "  <script src=\"assets/js/off-canvas.js\"></script>\n" +
-                "  <script src=\"assets/js/hoverable-collapse.js\"></script>\n" +
-                "  <script src=\"assets/js/misc.js\"></script>\n" +
-                "  <script src=\"assets/js/file-upload.js\"></script>\n" +
-                "  <script src=\"assets/js/typeahead.js\"></script>\n" +
-                "  <script src=\"assets/js/select2.js\"></script>\n" +
+                "  <script src=\"./assets/vendors/js/vendor.bundle.base.js\"></script>\n" +
+                "  <script src=\"./assets/vendors/select2/select2.min.js\"></script>\n" +
+                "  <script src=\"./assets/vendors/typeahead.js/typeahead.bundle.min.js\"></script>\n" +
+                "  <script src=\"./assets/js/off-canvas.js\"></script>\n" +
+                "  <script src=\"./assets/js/hoverable-collapse.js\"></script>\n" +
+                "  <script src=\"./assets/js/misc.js\"></script>\n" +
+                "  <script src=\"./assets/js/file-upload.js\"></script>\n" +
+                "  <script src=\"./assets/js/typeahead.js\"></script>\n" +
+                "  <script src=\"./assets/js/select2.js\"></script>\n" +
                 "</body>\n" +
                 "\n" +
                 "</html>";
