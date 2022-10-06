d82daef97162bbb03ca6738fff583bb6eff13ff9
simonwandera <simonwandera12@gmail.com>
2022-10-06 15:17:59 +0300

    Added side nav to common class

--- src/main/java/com/capacitybuilding/actions/Common.java
+++ src/main/java/com/capacitybuilding/actions/Common.java
@@ -33,4 +33,89 @@ public class Common {
                 "\n" +
                 "</html>";
     }
+
+    public static String SideNav(){
+        return  "      <nav class=\"sidebar sidebar-offcanvas\" id=\"sidebar\">\n" +
+                "        <div class=\"text-center sidebar-brand-wrapper d-flex align-items-center\">\n" +
+                "          <a class=\"sidebar-brand brand-logo\" href=\"index.html\"><img src=\"assets/images/logo.svg\" alt=\"logo\" /></a>\n" +
+                "          <a class=\"sidebar-brand brand-logo-mini pl-4 pt-3\" href=\"index.html\"><img src=\"assets/images/logo-mini.svg\" alt=\"logo\" /></a>\n" +
+                "        </div>\n" +
+                "        <ul class=\"nav\">\n" +
+                "          <li class=\"nav-item nav-profile\">\n" +
+                "            <a href=\"#\" class=\"nav-link\">\n" +
+                "              <div class=\"nav-profile-image\">\n" +
+                "                <img src=\"assets/images/faces/face1.jpg\" alt=\"profile\" />\n" +
+                "                <span class=\"login-status online\"></span>\n" +
+                "              </div>\n" +
+                "              <div class=\"nav-profile-text d-flex flex-column pr-3\">\n" +
+                "                <span class=\"font-weight-medium mb-2\">User</span>\n" +
+                "                <span class=\"font-weight-normal\">Admin</span>\n" +
+                "              </div>\n" +
+                "              <span class=\"badge badge-danger text-white ml-3 rounded\">3</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" href=\"index.html\">\n" +
+                "              <i class=\"mdi mdi-home menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Dashboard</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" data-toggle=\"collapse\" href=\"#ui-basic\" aria-expanded=\"false\" aria-controls=\"ui-basic\">\n" +
+                "              <i class=\"mdi mdi-crosshairs-gps menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Home</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" data-toggle=\"collapse\" href=\"#ui-basic\" aria-expanded=\"false\" aria-controls=\"ui-basic\">\n" +
+                "              <i class=\"mdi mdi-account-multiple menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Trainees</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          \n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" href=\"pages/icons/mdi.html\">\n" +
+                "              <i class=\"mdi mdi-contacts menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Notifications</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" href=\"pages/forms/basic_elements.html\">\n" +
+                "              <i class=\"mdi mdi-format-list-bulleted menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Report</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" href=\"pages/charts/chartjs.html\">\n" +
+                "              <i class=\"mdi mdi-chart-bar menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Trainings</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" href=\"pages/tables/basic-table.html\">\n" +
+                "              <i class=\"mdi mdi-table-large menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Graphs</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item\">\n" +
+                "            <a class=\"nav-link\" href=\"https://www.bootstrapdash.com/demo/breeze-free/documentation/documentation.html\">\n" +
+                "              <i class=\"mdi mdi-file-document-box menu-icon\"></i>\n" +
+                "              <span class=\"menu-title\">Documentation</span>\n" +
+                "            </a>\n" +
+                "          </li>\n" +
+                "          <li class=\"nav-item sidebar-actions\">\n" +
+                "            <div class=\"nav-link\">\n" +
+                "              <div class=\"mt-4\">\n" +
+                "                <div class=\"border-none\">\n" +
+                "                  <p class=\"text-black\">Settings</p>\n" +
+                "                </div>\n" +
+                "                <ul class=\"mt-4 pl-0\">\n" +
+                "                  <li><a href=\"#\">Sign Out</a></li>\n" +
+                "                </ul>\n" +
+                "              </div>\n" +
+                "            </div>\n" +
+                "          </li>\n" +
+                "        </ul>\n" +
+                "      </nav>";
+    }
 }
