72e8e5050aa79ea312e014ad69eba5bf03177612
simonwandera <simonwandera12@gmail.com>
2022-10-06 17:31:45 +0300

    -Added trainee class

--- src/main/java/com/capacitybuilding/actions/TraineeAction.java
+++ src/main/java/com/capacitybuilding/actions/TraineeAction.java
@@ -15,23 +15,6 @@ public class TraineeAction extends HttpServlet {
     }
 
     public String addTrainee(String actionError){
-        return "<!DOCTYPE html>"
-                + "<html> "
-                + "<head> "
-                + "</head>"
-                + "<body>"
-                + "<h1>" + getServletContext().getAttribute("applicationLabel") + "</h1>"
-                + "<h2> User Login</h2>"
-                + "<form action=\"./student\" method=\"post\">"
-                + "<table> "
-                + "<tr> <td> Student Name: </td> <td> <input type=\"text\" name=\"name\"> </td> </tr> "
-                + "<tr> <td> Student Reg No: </td> <td> <input type=\"text\" name=\"regNo\"> </td> </tr> "
-                + "<tr> <td> <input type=\"submit\" value=\"Submit\"></tr> "
-                + "</table>"
-                + "</form>"
-                + "<span style=\"color:red\">" + (actionError != null? actionError : "") + "</span><br/>"
-                + "Home? <a href='./home'>Register</a><br/>"
-                + "</body>"
-                + "</html>";
+        return "";
     }
 }
---
+++ src/main/java/com/capacitybuilding/model/Trainee.java
@@ -0,0 +1,4 @@
+package com.capacitybuilding.model;
+
+public class Trainee {
+}
