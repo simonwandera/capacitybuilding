686270bd159af0d1582a50330064e0fd11b9a686
simonwandera <simonwandera12@gmail.com>
2022-10-06 08:29:21 +0300

    -Request attribute listener

--- src/main/java/com/capacitybuilding/listeners/RequestAttributeListener.java
+++ src/main/java/com/capacitybuilding/listeners/RequestAttributeListener.java
@@ -1,4 +1,19 @@
 package com.capacitybuilding.listeners;
 
-public class RequestAttributeListener {
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.ServletRequestAttributeListener;
+
+public class RequestAttributeListener implements ServletRequestAttributeListener {
+
+    public void attributeAdded(ServletRequestAttributeEvent event) {
+        System.out.println("Added request attribute name:\t{"+event.getName()+",event value: \t"+event.getValue()+"}");
+    }
+
+    public void attributeRemoved(ServletRequestAttributeEvent event) {
+        System.out.println("Removed request attribute name: \t{"+event.getName()+", event value: \t"+event.getValue()+"}");
+    }
+
+    public void attributeReplaced(ServletRequestAttributeEvent event) {
+        System.out.println("Replaced request attribute name: \t{"+event.getName()+", event value: \t"+event.getValue()+"}");
+    }
 }
