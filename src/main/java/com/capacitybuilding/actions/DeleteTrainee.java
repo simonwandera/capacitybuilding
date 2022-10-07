cab5bb63b4a379cc1571be69627cf86f38c6815a
simonwandera <simonwandera12@gmail.com>
2022-10-07 06:48:05 +0300

    Get deleted trainee attributes

--- src/main/java/com/capacitybuilding/actions/DeleteTrainee.java
+++ src/main/java/com/capacitybuilding/actions/DeleteTrainee.java
@@ -1,7 +1,26 @@
 package com.capacitybuilding.actions;
 
-import javax.servlet.annotation.WebServlet;
+import com.capacitybuilding.model.Trainee;
 
-@WebServlet(urlPatterns = "/logout"
-public class DeleteTrainee {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/deleteTrainee")
+public class DeleteTrainee extends HttpServlet {
+
+    @SuppressWarnings("unchecked")
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        System.out.println("Deleting trainee  >> " + req.getParameter("id"));
+
+        HttpSession session = req.getSession();
+        List<Trainee> trainees = (List<Trainee>) session.getAttribute("trainees");
+        res.sendRedirect("./trainees");
+    }
 }
