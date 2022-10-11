145a9506049171e51d44e23be5e8b316b22f6578
simonwandera <simonwandera12@gmail.com>
2022-10-11 22:49:15 +0300

    Added a reusable mysql interface entity

--- src/main/java/com/capacitybuilding/Service/Entity.java
+++ src/main/java/com/capacitybuilding/Service/Entity.java
@@ -1,13 +1,18 @@
 package com.capacitybuilding.Service;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 public class Entity {
     private String tableName;
-
     private Map<String, Object> entitiesMap;
 
     public Map<String, Object> getEntitiesMap() {
         return entitiesMap;
+    }
+
+    public IMySQLDB<Entity> getMySqlDB() throws SQLException {
+        return new MySQLDB<>(new Entity(this.entitiesMap, this.tableName));
     }
 
     public String getTableName() {
@@ -17,6 +22,14 @@ public class Entity {
     public Entity(Map<String, Object> entitiesMap, String tableName) {
         this.tableName = tableName;
         this.entitiesMap = entitiesMap;
+    }
+
+    public ResultSet getAll() throws SQLException {
+        return this.getMySqlDB().fetchAll();
+    }
+
+    public ResultSet getOne() throws SQLException {
+        return this.getMySqlDB().fetchOne();
     }
 
 }
