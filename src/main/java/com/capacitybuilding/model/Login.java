ff4e95947dbe3a467b4f8b56ad9c9fcc52b56724
simonwandera <simonwandera12@gmail.com>
2022-10-11 12:47:14 +0300

    Added getters and settes for login class

--- src/main/java/com/capacitybuilding/model/Login.java
+++ src/main/java/com/capacitybuilding/model/Login.java
@@ -1,21 +1,69 @@
 package com.capacitybuilding.model;
 
 import com.capacitybuilding.Service.Entity;
+import com.capacitybuilding.Service.IMySQLDB;
+import com.capacitybuilding.Service.MySQLDB;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Login extends Entity {
 
     private int Id;
-    private String UserName;
+    private String Username;
     private String Password;
     private String UserType;
 
     static ResultSet resultSet;
-    private static final String tableName = "student";
+    private static final String tableName = "Login";
 
-    public Login(Map<String, Object> entitiesMap, String tableName) {
+    private static final Map<String, Object> entitiesMap = new HashMap<>(){{
+        put("Id", null);
+        put("Username", "");
+        put("Password", "");
+        put("UserType", "");
+    }};;
+
+    public Login() throws SQLException {
         super(entitiesMap, tableName);
+    }
+
+    public static IMySQLDB<Login> getLoginDB() throws SQLException {
+        return new MySQLDB<>(new Login());
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        entitiesMap.put("Id", id);
+        Id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getUserType() {
+        return UserType;
+    }
+
+    public void setUserType(String userType) {
+        UserType = userType;
     }
 }
