70d115651e2684dbcf86920e2c41c7abb019c9fd
simonwandera <simonwandera12@gmail.com>
2022-10-11 12:14:17 +0300

    Added an entity class

---
+++ src/main/java/com/capacitybuilding/Service/Entity.java
@@ -0,0 +1,22 @@
+package com.capacitybuilding.Service;
+import java.util.Map;
+
+public class Entity {
+    private String tableName;
+
+    private Map<String, Object> entitiesMap;
+
+    public Map<String, Object> getEntitiesMap() {
+        return entitiesMap;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public Entity(Map<String, Object> entitiesMap, String tableName) {
+        this.tableName = tableName;
+        this.entitiesMap = entitiesMap;
+    }
+
+}
