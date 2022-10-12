01b5b7257b664b93c441a63748aa70d3a47c3cf3
simonwandera <simonwandera12@gmail.com>
2022-10-12 15:57:16 +0300

    -Added connection to servelet context

--- src/main/java/com/capacitybuilding/listeners/ContextListener.java
+++ src/main/java/com/capacitybuilding/listeners/ContextListener.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
@@ -12,5 +14,28 @@ public class ContextListener implements ServletContextListener {
         System.out.print("Capacity building system... initializing default attributes");
         ServletContext servletContext = event.getServletContext();
         servletContext.setAttribute("appName", "Capacity building system | Made in Kenya");
+
+        try {
+            System.out.print("Establishing connections....");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CapacityBuilding",
+                    "root", "");
+            servletContext.setAttribute("dbConnection", connection);
+            System.out.print("Connection Established....");
+        } catch (Exception ex) {
+            System.out.println("Connection Note Established....: " + ex.getMessage());
+
+        }
     }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        try {
+            ServletContext ctx = servletContextEvent.getServletContext();
+            Connection connection = (Connection) ctx.getAttribute("dbConnection");
+            connection.close();
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
