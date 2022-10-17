5bd6477462c2b50b1bf7bac392d9166f113684f4
simonwandera <simonwandera12@gmail.com>
2022-10-17 15:31:59 +0300

    Modelled trainee controller

--- src/main/java/com/capacitybuilding/controllers/TraineeController.java
+++ src/main/java/com/capacitybuilding/controllers/TraineeController.java
@@ -1,6 +1,29 @@
 package com.capacitybuilding.controllers;
 
+import com.capacitybuilding.model.Trainee;
+
 import java.io.Serializable;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TraineeController implements Serializable {
+
+    public void add(Connection connection, Trainee trainee){
+
+    }
+
+    public void update(Trainee trainee) {
+
+    }
+
+    public void delete(Trainee trainee) {
+
+    }
+
+    public List<Trainee> list(Connection connection, Trainee trainee){
+        List<Trainee> trainees = new ArrayList<>();
+        return trainees;
+    }
+
 }
