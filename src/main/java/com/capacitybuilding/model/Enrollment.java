8b804885ee52fa902e3a25d5694bf6f7b9081383
simonwandera <simonwandera12@gmail.com>
2022-10-24 21:39:58 +0300

    Enrollment model

--- src/main/java/com/capacitybuilding/model/Enrollment.java
+++ src/main/java/com/capacitybuilding/model/Enrollment.java
@@ -2,6 +2,7 @@ package com.capacitybuilding.model;
 
 import com.capacitybuilding.Service.Entity;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,19 +10,68 @@ import java.util.Map;
 
 public class Enrollment extends Entity {
     private int Id;
+    private Date dateEnrolled;
+    private String status;
+    private User trainee;
+    private Training training;
 
-    static final String tableName = "Training";
+
+    static final String tableName = "Enrollment";
 
     static final Map<String, Object> entitiesMap = new HashMap<>(){{
         put("Id", null);
-        put("Title", "");
-        put("Description", "");
-        put("Duration", null);
-        put("StartDate", null);
-        put("DateAdded", null);
+        put("TrainingId", null);
+        put("TraineeId ", null);
+        put("DateEnrolled", null);
+        put("Status ", "");
     }};;
 
     public Enrollment() {
         super(entitiesMap, tableName);
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        entitiesMap.put("Id", id);
+        Id = id;
+    }
+
+    public Date getDateEnrolled() {
+        return dateEnrolled;
+    }
+
+    public void setDateEnrolled(Date dateEnrolled) {
+        entitiesMap.put("DateEnrolled", dateEnrolled);
+        this.dateEnrolled = dateEnrolled;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        entitiesMap.put("Status", status);
+        this.status = status;
+    }
+
+    public User getTrainee() {
+        return trainee;
+    }
+
+    public void setTrainee(User trainee) {
+        entitiesMap.put("TraineeId", trainee.getId());
+        this.trainee = trainee;
+    }
+
+    public Training getTraining() {
+        return training;
+    }
+
+    public void setTraining(Training training) {
+        entitiesMap.put("TrainingId", training.getId());
+        this.training = training;
     }
 }
