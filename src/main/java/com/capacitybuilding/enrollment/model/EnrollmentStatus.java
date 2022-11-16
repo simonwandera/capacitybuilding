435f72aac169dfc862831a73f4473fd5a14e136c
simonwandera <simonwandera12@gmail.com>
2022-11-16 14:02:32 +0300

    Edded enrollment enum

---
+++ src/main/java/com/capacitybuilding/enrollment/model/EnrollmentStatus.java
@@ -0,0 +1,9 @@
+package com.capacitybuilding.enrollment.model;
+
+public enum EnrollmentStatus {
+    APPROVED("APPROVED"),
+    DECLINED("DECLINED");
+
+    EnrollmentStatus(String status) {
+    }
+}
