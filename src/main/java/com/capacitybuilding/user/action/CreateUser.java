4e86d78909c1bc0d49728d8dce7c4228672da13f
simonwandera <simonwandera12@gmail.com>
2022-11-21 13:44:46 +0300

    Created a create user action

--- src/main/java/com/capacitybuilding/user/action/CreateUser.java
+++ src/main/java/com/capacitybuilding/user/action/CreateUser.java
@@ -1,6 +1,41 @@
 package com.capacitybuilding.user.action;
 
+import com.capacitybuilding.user.model.User;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class CreateUser extends HttpServlet {
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        User user = new User();
+
+        try {
+            BeanUtils.populate(user, req.getParameterMap());
+
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println(user);
+
+//        try {
+//            userBean.register(user);
+//            res.sendRedirect("./login.jsp");
+//
+//        } catch (Exception ex) {
+//            servletCtx.setAttribute("registerError" , ex.getMessage());
+//            res.sendRedirect("./register.jsp");
+//        }
+
+    }
+
+
+
+
 }
