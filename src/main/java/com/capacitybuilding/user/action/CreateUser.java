97d3cbfebf66f791559457b6c4c6b0465cf89f94
simonwandera <simonwandera12@gmail.com>
2022-11-21 14:37:56 +0300

    Servlet do init for configuration

--- src/main/java/com/capacitybuilding/user/action/CreateUser.java
+++ src/main/java/com/capacitybuilding/user/action/CreateUser.java
@@ -7,6 +7,7 @@ import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import javax.ejb.EJB;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,6 +24,11 @@ public class CreateUser extends HttpServlet {
 
     ServletContext servletContext;
 
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        servletContext = getServletConfig().getServletContext();
+    }
+    
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
         User user = new User();
@@ -38,11 +44,13 @@ public class CreateUser extends HttpServlet {
         user.setPassword(DigestUtils.md5Hex(user.getPassword()));
         user.setConfirmPassword(DigestUtils.md5Hex(user.getConfirmPassword()));
 
+        System.out.println(user);
+
         try {
             userBean.add(user);
             res.sendRedirect("./admin/users.jsp");
         } catch (Exception ex) {
-            servletContext.setAttribute("registerError", ex.getMessage());
+            servletContext.setAttribute("addUserError", ex.getMessage());
             res.sendRedirect("./addUser.jsp");
         }
 
