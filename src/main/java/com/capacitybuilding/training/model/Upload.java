5bc645ab544ce8ade1259d23e571c9af11ad749f
simonwandera <simonwandera12@gmail.com>
2022-11-22 21:51:46 +0300

    Uploads model

--- src/main/java/com/capacitybuilding/training/model/Upload.java
+++ src/main/java/com/capacitybuilding/training/model/Upload.java
@@ -2,5 +2,33 @@ package com.capacitybuilding.training.model;
 
 import com.capacitybuilding.util.model.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class Upload extends BaseEntity {
+
+    @Column
+    private String fileName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "training_id", referencedColumnName = "id")
+    private Training training;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Training getTraining() {
+        return training;
+    }
+
+    public void setTraining(Training training) {
+        this.training = training;
+    }
 }
